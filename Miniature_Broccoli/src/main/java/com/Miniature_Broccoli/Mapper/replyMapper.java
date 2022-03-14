7954ef105ca8a4837bb93fb78c590ea4df42d4fe
小目 <65577896+Scc00001@users.noreply.github.com>
2022-03-14 17:10:22 +0800

    Revert "初步版本"
    This reverts commit 842e9e9da6b3cb8cd95f8fe38edd4d7ff12384af.

--- Miniature_Broccoli/src/main/java/com/Miniature_Broccoli/Mapper/replyMapper.java
+++
@@ -1,8 +0,0 @@
-package com.Miniature_Broccoli.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface replyMapper extends BaseMapper<replyMapper> {
-}
