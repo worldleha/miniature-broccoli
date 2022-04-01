6563cc18e7340f1a83d58d889572e30a83eacf5a
worldleha <worldleha@outlook.com>
2022-04-01 11:05:49 +0800

    删减优化

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,59 @@
 <template>
-	
+
 	<div id="show_window">
-		<VTop class = "top-header"></VTop>
+		<TopNavigation class="top-header"></TopNavigation>
 		<!-- //暂时未用于滑动窗口 -->
-		<SlidingWindow 
-		id= "move_page" 
-		:style="{transform: 'translateY('+position+'px)'}" 
-		backgroundColor = "red">
-			<template v-slot:default>	
-				<FirstShow/>
+		<SlidingWindow id="move_page" :style="{transform: 'translateY('+position+'px)'}" backgroundColor="red">
+			<template v-slot:default>
+				<TopWindow></TopWindow>
 			</template>
-			
+
 		</SlidingWindow>
-		<SlidingWindow backgroundColor = "green"></SlidingWindow>
-		<SlidingWindow backgroundColor = "blue"></SlidingWindow>
-		
+		<SlidingWindow backgroundColor="green">
+			<template>
+				<MiddleWindow></MiddleWindow>
+			</template>
+		</SlidingWindow>
+		<SlidingWindow backgroundColor="blue">
+			<template>
+				<BottomWindow></BottomWindow>
+			</template>
+		</SlidingWindow>
+
 	</div>
-	
+
 </template>
 
 <script>
-import SlidingWindow from "./components/SlidingWindow"
-import VTop from "./components/VTop"
-import FirstShow from "./components/FirstShow"
-
-export default {
+	
+	
+	import SlidingWindow from "./components/SlidingWindow"
+	import TopNavigation from "./components/TopNavigation"
+	import TopWindow from "./components/TopWindow"
+	import MiddleWindow from "./components/MiddleWindow"
+	import BottomWindow from "./components/BottomWindow"
+	
+	
+	export default {
 		name: 'App',
 		components: {
 			SlidingWindow,
-			VTop,
-			FirstShow,
+			TopNavigation,
+			TopWindow,
+			MiddleWindow,
+			BottomWindow
 		},
-		data(){
-			return{
+		data() {
+			return {
 				//暂时未用于滑动窗口
-				position: 0
+				position: 0,
+				objtest:{on:"ssd"}
 			}
 		},
-		methods:{
+		
+		methods: {
 			//暂时未用于滑动窗口
-			toPosition(pos){
+			toPosition(pos) {
 				this.position = pos
 			}
 		},
@@ -56,30 +70,29 @@ export default {
 
 <style scoped>
 	/* @import url("../public/css/reset.css"); */
-	#show_window{
-		
+	#show_window {
+
 		position: absolute;
-		
+
 		width: 100%;
 		height: 100vh;
-		
-		
+
+
 		/* overflow: hidden; */
 	}
-	
-	#move_page{
+
+	#move_page {
 		transform: translateY(0px);
 	}
-	
-	.top-header{
-		
+
+	.top-header {
+
 		position: fixed;
-		
+
 		width: 100vw;
-	
+
 		background-color: aqua;
-		
+
 		z-index: 12;
 	}
-
 </style>
