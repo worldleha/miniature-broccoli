a85e283aa732bf4c8ba6269dfb2672cc7d64467a
worldleha <worldleha@outlook.com>
2022-04-01 11:06:20 +0800

    最后一个窗口

---
+++ src/components/BottomWindow.vue
@@ -0,0 +1,27 @@
+<template>
+	<transition name="fade">
+		<div v-show="index === 2" class="middle-window">
+			<ShowArticle v-if="page.page3 === 1"></ShowArticle>
+		</div>
+	</transition>
+
+</template>
+
+<script>
+	import {mapState} from "vuex"
+	import ShowArticle from "../pages/ShowArticle"
+
+	
+	
+	export default {
+		name: "BottomWindow",
+		components:{
+			ShowArticle,
+		},
+		computed:{
+			...mapState("pageControlOption",["index", "page"])
+		}
+	}
+</script>
+<style>
+</style>
