814cd4362602ad58e47f5591d226d6afde78e481
worldleha <worldleha@outlook.com>
2022-04-01 11:13:06 +0800

    更改store

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,16 @@
-export default{
-	'/': ''
-}
+import Vue from 'vue'
+import Vuex from "vuex"
+
+import pageControlOption from "./pageControlOption.js"
+import articleHeaderOption from "./articleHeaderOption.js"
+
+Vue.use(Vuex)
+
+
+
+export default new Vuex.Store({
+	modules:{
+		pageControlOption,
+		articleHeaderOption
+	}
+})
